8edd9e8cb0ae020a7f09ea04f8aaa7d3573714ec
Dominique Huber <dominique@donhubi.ch>
2017-09-16 22:23:36 +0200

    Remove comments

--- public/webworker.js
+++ public/webworker.js
@@ -1,8 +1,8 @@
 self.importScripts('crypto.js');
-self.addEventListener('message', function (e) {
+self.addEventListener('message', function(e) {
   if (e.data.message === 'Start') {
     console.log('Start hasing!! pew pew!');
-    self.postMessage({message: 'message from worker'});
+    self.postMessage({ message: 'message from worker' });
     Mining.start();
   }
 }, false);
@@ -12,7 +12,7 @@ self.addEventListener('message', function (e) {
 var difficulty = 4;        // number of zeros required at front of hash
 var maximumNonce = 500000; // limit the nonce to this so we don't mine too long
 
-// NOTE: Because there are 16 possible characters in a hex value, each time you incrament
+// NOTE: Because there are 16 possible characters in a hex value, each time you increment
 // the difficulty by one you make the puzzle 16 times harder. In my testing, a difficulty
 // of 6 requires a maximumNonce well over 500,000,000.
 
@@ -20,7 +20,7 @@ var maximumNonce = 500000; // limit the nonce to this so we don't mine too long
 // global variable setup
 /////////////////////////
 var pattern = '';
-for (var x=0; x<difficulty; x++) {
+for (var x = 0; x < difficulty; x++) {
   pattern += '0';
 }
 
@@ -33,23 +33,14 @@ function sha256(block) {
 }
 
 function updateState(block, blockId, performance) {
-    console.log(block);
-    console.log('block ' + block + ' blockId' + blockId);
-    self.postMessage({block: block, blockId: blockId, performance: performance});
-
-
-    // set the well background red or green for this block
-    // if ($('#block'+block+'chain'+chain+'hash').val().substr(0, difficulty) === pattern) {
-    //   $('#block'+block+'chain'+chain+'well').removeClass('well-error').addClass('well-success');
-    // }
-    // else {
-    //   $('#block'+block+'chain'+chain+'well').removeClass('well-success').addClass('well-error');
-    // }
+  console.log(block);
+  console.log('block ' + block + ' blockId' + blockId);
+  self.postMessage({ block: block, blockId: blockId, performance: performance });
 }
 
 function updateHash(block, chain) {
   // update the SHA256 hash value for this block
-  $('#block'+block+'chain'+chain+'hash').val(sha256(block, chain));
+  $('#block' + block + 'chain' + chain + 'hash').val(sha256(block, chain));
   //updateState(block, chain);
 }
 
@@ -57,7 +48,7 @@ function updateChain(block, chain) {
   // update all blocks walking the chain from this block to the end
   for (var x = block; x <= 5; x++) {
     if (x > 1) {
-      $('#block'+x+'chain'+chain+'previous').val($('#block'+(x-1).toString()+'chain'+chain+'hash').val());
+      $('#block' + x + 'chain' + chain + 'previous').val($('#block' + (x - 1).toString() + 'chain' + chain + 'hash').val());
 
     }
     updateHash(x, chain);
@@ -72,23 +63,23 @@ function mine(blockId, previousHash, data) {
     if (computedBlock.substr(0, difficulty) === pattern) {
       console.log(x)
       var end = new Date().getTime();
-        var elapsedTime = (end - start);
-        console.log("Hash per secs");
-        let performance = x  /  elapsedTime * 1000;
-        console.log(performance);
-        updateState( block, blockId, performance);
-        restartMining(blockId + 1, computedBlock, 'fooBar');
+      var elapsedTime = (end - start);
+      console.log("Hash per secs");
+      let performance = x / elapsedTime * 1000;
+      console.log(performance);
+      updateState(block, blockId, performance);
+      restartMining(blockId + 1, computedBlock, 'fooBar');
       break;
     }
   }
 }
 
-function restartMining (blockId, previousHash, data) {
+function restartMining(blockId, previousHash, data) {
   mine(blockId, previousHash, data)
 }
 
 var Mining = {
-  start: function () {
+  start: function() {
     mine(1, '0000000000000000000000000000000000000000000000000000000000000000', 'fooBar');
   }
 };
